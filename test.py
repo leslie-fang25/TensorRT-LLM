819faca8c64804c75465dfbf4515ce19a77d6ff3
leslie-fang25 <leslief@nvidia.com>
2025-07-16 20:10:31 -0700

    test push

---
+++ test.py
@@ -0,0 +1,28 @@
+from tensorrt_llm import SamplingParams
+from tensorrt_llm._tensorrt_engine import LLM
+
+
+def main():
+
+    prompts = [
+        "Hello, my name is",
+        "The capital of France is",
+        "The future of AI is",
+    ]
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+    # Alternatively, use "nvidia/Llama-3.1-8B-Instruct-FP8" to enable FP8 inference.
+    llm = LLM(model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+
+    outputs = llm.generate(prompts, sampling_params)
+
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+
+# The entry point of the program need to be protected for spawning processes.
+if __name__ == '__main__':
+    main()
