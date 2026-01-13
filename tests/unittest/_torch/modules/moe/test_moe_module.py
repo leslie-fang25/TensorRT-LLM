75344dda3f2d796095e0f1ac83a90fd669a7abd3
leslie-fang25 <leslief@nvidia.com>
2026-01-12 21:37:15 -0800

    [None][chore] Add test configurable moe module multi gpu
    Signed-off-by: leslie-fang25 <leslief@nvidia.com>

--- tests/unittest/_torch/modules/moe/test_moe_module.py
+++ tests/unittest/_torch/modules/moe/test_moe_module.py
@@ -13,10 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+import pickle
+import sys
 
+import cloudpickle
 import pytest
 import torch
 from _torch.modules.moe.quantize_utils import get_test_quant_params
+from mpi4py import MPI
+from mpi4py.futures import MPIPoolExecutor
 from transformers.configuration_utils import PretrainedConfig
 from utils.util import getSMVersion
 
@@ -25,6 +30,89 @@ from tensorrt_llm._torch.modules.fused_moe import RenormalizeMoeRoutingMethod, c
 from tensorrt_llm._utils import mpi_rank
 from tensorrt_llm.mapping import Mapping
 from tensorrt_llm.models.modeling_utils import QuantAlgo
+
+cloudpickle.register_pickle_by_value(sys.modules[__name__])
+MPI.pickle.__init__(
+    cloudpickle.dumps,
+    cloudpickle.loads,
+    pickle.HIGHEST_PROTOCOL,
+)
+
+
+def test_moe_worker(moe_backend, dtype, quant_algo, mapping=None):
+    # Hardcode some parameters for testing
+    # activation and weight related
+    seq_len = 4
+    top_k = 2
+    num_experts = 8
+    hidden_size = 512
+    intermediate_size = 512
+
+    # Other parameters
+    finalize_fusion = True
+
+    mapping = mapping or Mapping()
+    mapping.rank = mpi_rank()
+
+    all_rank_num_tokens = [seq_len] * mapping.world_size
+
+    torch.cuda.set_device(mapping.rank)
+
+    with torch.device(f"cuda:{mapping.rank}"):
+        torch.manual_seed(0)
+        torch.cuda.manual_seed(0)
+
+        # Create route method
+        routing_method = RenormalizeMoeRoutingMethod(top_k=top_k)
+
+        # Create activation and weight
+        x = torch.randn((seq_len, hidden_size), dtype=dtype, device="cuda")
+        router_logits = torch.randn((seq_len, num_experts), dtype=dtype, device="cuda")
+
+        quantize_util_cls, quant_config, quant_kwargs = get_test_quant_params(quant_algo, x)
+        quantize_util = quantize_util_cls(
+            num_experts=num_experts,
+            dtype=dtype,
+            intermediate_size=intermediate_size,
+            hidden_size=hidden_size,
+            quant_config=quant_config,
+        )
+        weights = quantize_util.create_weights(**quant_kwargs)
+
+        # Create pretrained config
+        pretrained_config = PretrainedConfig()
+        pretrained_config.num_experts = num_experts
+        pretrained_config.hidden_size = hidden_size
+        pretrained_config.intermediate_size = intermediate_size
+        pretrained_config.torch_dtype = dtype
+
+        # Create fused MoE module
+        fused_moe = create_moe(
+            routing_method=routing_method,
+            reduce_results=True,
+            model_config=ModelConfig(
+                pretrained_config=pretrained_config,
+                mapping=mapping,
+                quant_config=quant_config,
+                moe_backend=moe_backend,
+                moe_disable_finalize_fusion=not finalize_fusion,
+            ),
+        )
+
+        fused_moe.load_weights([weights])
+        fused_moe.post_load_weights()
+        fused_moe.cuda()
+
+        ref_fused_moe = quantize_util.create_ref_module(routing_method)
+        ref_fused_moe.load_weights([weights])
+        ref_fused_moe.cuda()
+
+        # Evaluate the outputs
+        with torch.inference_mode():
+            ref_output = ref_fused_moe.forward(x, router_logits)
+            output = fused_moe.forward(x, router_logits, all_rank_num_tokens=all_rank_num_tokens)
+
+        ref_fused_moe.check_accuracy(output, ref_output)
 
 
 @pytest.mark.parametrize(
@@ -61,71 +149,76 @@ def test_moe(dtype, moe_backend, quant_algo, mocker):
     if quant_algo == QuantAlgo.NVFP4 and getSMVersion() < 100:
         pytest.skip("This test is not supported in pre-Blackwell architecture")
 
-    # Hardcode some parameters for testing
-    # activation and weight related
-    seq_len = 4
-    top_k = 2
-    num_experts = 8
-    hidden_size = 512
-    intermediate_size = 512
-    # Other parameters
-    finalize_fusion = True
+    test_moe_worker(moe_backend=moe_backend, dtype=dtype, quant_algo=quant_algo)
 
-    # Create mapping for current rank
-    mapping = Mapping()
-    mapping.rank = mpi_rank()
 
-    with torch.device(f"cuda:{mapping.rank}"):
-        torch.manual_seed(0)
-        torch.cuda.manual_seed(0)
-        # Create route method
-        routing_method = RenormalizeMoeRoutingMethod(top_k=top_k)
+@pytest.mark.skipif(torch.cuda.device_count() < 4, reason="needs 4 GPUs to run this test")
+@pytest.mark.parametrize(
+    "quant_algo",
+    [
+        None,
+        QuantAlgo.NVFP4,
+    ],
+    ids=lambda val: f"quant_algo={val}",
+)
+@pytest.mark.parametrize(
+    "moe_backend",
+    [
+        "CUTLASS",
+        "TRTLLM",
+    ],
+    ids=lambda val: f"moe_backend={val}",
+)
+@pytest.mark.parametrize(
+    "alltoall_method_type",
+    [
+        "NVLINK_ONE_SIDED",
+        "NVLINK_TWO_SIDED",
+    ],
+    ids=lambda val: f"alltoall_method_type={val}",
+)
+def test_moe_multi_gpu(alltoall_method_type, moe_backend, quant_algo):
+    if quant_algo == QuantAlgo.NVFP4 and getSMVersion() < 100:
+        pytest.skip("This test is not supported in pre-Blackwell architecture")
 
-        # Create activation and weight
-        x = torch.randn((seq_len, hidden_size), dtype=dtype, device="cuda")
-        router_logits = torch.randn((seq_len, num_experts), dtype=dtype, device="cuda")
+    dtype = torch.bfloat16
+    ep_size = 4
+    world_size = 4
 
-        quantize_util_cls, quant_config, quant_kwargs = get_test_quant_params(quant_algo, x)
+    def init_worker(custom_paths, alltoall_method_type):
+        # Update the sys.path to align with main process for submodule import
+        for custom_path in custom_paths:
+            if custom_path.endswith("tests/unittest") and custom_path not in sys.path:
+                sys.path.append(custom_path)
 
-        quantize_util = quantize_util_cls(
-            num_experts=num_experts,
-            dtype=dtype,
-            intermediate_size=intermediate_size,
-            hidden_size=hidden_size,
-            quant_config=quant_config,
-        )
+        # Enable configurable moe by default
+        os.environ["ENABLE_CONFIGURABLE_MOE"] = "1"
 
-        weights = quantize_util.create_weights(**quant_kwargs)
+        # Set comm method
+        os.environ["TRTLLM_FORCE_COMM_METHOD"] = alltoall_method_type
 
-        # Create pretrained config
-        pretrained_config = PretrainedConfig()
-        pretrained_config.num_experts = num_experts
-        pretrained_config.hidden_size = hidden_size
-        pretrained_config.intermediate_size = intermediate_size
-        pretrained_config.torch_dtype = dtype
-
-        # Create fused MoE module
-        fused_moe = create_moe(
-            routing_method=routing_method,
-            reduce_results=True,
-            model_config=ModelConfig(
-                pretrained_config=pretrained_config,
-                quant_config=quant_config,
-                moe_backend=moe_backend,
-                moe_disable_finalize_fusion=not finalize_fusion,
+    with MPIPoolExecutor(
+        initializer=init_worker, initargs=(sys.path, alltoall_method_type), max_workers=world_size
+    ) as executor:
+        results = executor.map(
+            test_moe_worker,
+            *zip(
+                *[
+                    (
+                        moe_backend,
+                        dtype,
+                        quant_algo,
+                        Mapping(
+                            world_size=world_size,
+                            tp_size=world_size,
+                            moe_ep_size=ep_size,
+                            moe_tp_size=world_size // ep_size,
+                            enable_attention_dp=True,
+                        ),
+                    )
+                ]
+                * world_size
             ),
         )
-        fused_moe.load_weights([weights])
-        fused_moe.post_load_weights()
-        fused_moe.cuda()
-
-        ref_fused_moe = quantize_util.create_ref_module(routing_method)
-        ref_fused_moe.load_weights([weights])
-        ref_fused_moe.cuda()
-
-        # Evaluate the outputs
-        with torch.inference_mode():
-            ref_output = ref_fused_moe.forward(x, router_logits)
-            output = fused_moe.forward(x, router_logits)
-
-        ref_fused_moe.check_accuracy(output, ref_output)
+        for r in results:
+            assert r is None
